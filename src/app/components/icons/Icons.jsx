492f6a57a36317ab76e98245e1a495eb54a3236f
serhat <serhatciftci@gmail.com>
2021-01-20 23:12:06 +0100

    Add new icons

--- src/app/components/icons/Icons.jsx
+++ src/app/components/icons/Icons.jsx
@@ -30,22 +30,76 @@ export const SettingsIcon = () => {
     ? "bi bi-x-circle-fill red icon"
     : "bi bi-gear-fill white icon";
 
-  return <i className={iconType} onClick={useSettingsUpdate()}></i>;
+  return (
+    <i
+      className={iconType}
+      id="setting-icon"
+      alt="settings"
+      onClick={useSettingsUpdate()}
+    ></i>
+  );
 };
 
 export const VolumeIcon = () => {
   const user = useVolume();
   const iconType = user
-    ? "bi bi-volume-mute-fill red settings-icon"
-    : "bi bi-volume-up-fill white settings-icon";
-  return <i className={iconType} onClick={useVolumeUpdate()}></i>;
+    ? "bi bi-volume-mute-fill dark-red icon medium"
+    : "bi bi-volume-up-fill white icon medium";
+  return (
+    <i
+      className={iconType}
+      id="volume-icon"
+      alt="volume"
+      onClick={useVolumeUpdate()}
+    ></i>
+  );
 };
 
 export const NightModeIcon = () => {
   const user = useNightMode();
   const iconType = user
-    ? "bi bi-moon white small-icon"
-    : "bi bi-brightness-high white small-icon";
+    ? "bi bi-brightness-high white icon adjusted"
+    : "bi bi-moon dark-red icon adjusted";
 
-  return <i className={iconType} onClick={useNightModeUpdate()}></i>;
+  return (
+    <i
+      className={iconType}
+      id="night-mode-icon"
+      alt="nightMode"
+      onClick={useNightModeUpdate()}
+    ></i>
+  );
+};
+
+export const FlipIcon = (props) => {
+  return (
+    <i
+      className={props.styles}
+      id="flip-icon"
+      alt="flip"
+      onClick={() => props.func()}
+    ></i>
+  );
+};
+
+export const CorrectIcon = (props) => {
+  return (
+    <i
+      className={props.styles}
+      id="correct-icon"
+      alt="correct"
+      onClick={() => props.func(true)}
+    ></i>
+  );
+};
+
+export const WrongIcon = (props) => {
+  return (
+    <i
+      className={props.styles}
+      id="wrong-icon"
+      alt="wrong"
+      onClick={() => props.func(false)}
+    ></i>
+  );
 };
