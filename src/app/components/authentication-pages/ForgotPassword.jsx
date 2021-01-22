4686be2032fc73b18b1d05929d47e898216595f9
serhat <serhatciftci@gmail.com>
2021-01-22 15:51:22 +0100

    Convert bootstrap to custom style

--- src/app/components/authentication-pages/ForgotPassword.jsx
+++ src/app/components/authentication-pages/ForgotPassword.jsx
@@ -1,54 +1,27 @@
-import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
-import { useAuth } from "../../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import "./authentication.css";
+
+import Input from "../form-components/Input";
+import Form from "../form-components/Form";
+import FormButton from "../form-components/FormButton";
 
 export default function ForgotPassword() {
-  const emailRef = useRef();
-  const { resetPassword } = useAuth();
-  const [error, setError] = useState("");
-  const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    try {
-      setMessage("");
-      setError("");
-      setLoading(true);
-      await resetPassword(emailRef.current.value);
-      setMessage("Check your inbox for further instructions");
-    } catch {
-      setError("Failed to reset password");
-    }
-
-    setLoading(false);
-  }
-
   return (
-    <div className="authentication-container">
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-2">Password Reset</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Reset Password
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
+    <div className="auth-container">
+      <Form id="password-reset" method="post" legend="RESET PASSWORD">
+        <Input label="Email" id="email" type="email" />
+        <FormButton title="Submit" type="submit" />
+      </Form>
+      <div className="links-container">
+        Do you need to{" "}
+        <Link to="/signup">
+          <strong>Sign Up?</strong>
+        </Link>
+        <br></br>
+        or want to{" "}
+        <Link to="/Login">
+          <strong>Log In?</strong>
+        </Link>
       </div>
     </div>
   );
