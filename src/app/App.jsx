def380aa55c014401074cef876a6eb0aa48134a7
serhat <serhatciftci@gmail.com>
2021-01-25 22:09:04 +0100

    Add home page data fetching and loading

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,41 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SettingsProvider } from "./contexts/SettingsContext";
 import { useTheme } from "./contexts/ThemeContext";
-import { Switch, Route } from "react-router-dom";
 import "./app.css";
 
 // Main App components
 import Header from "./components/header-footer/Header.jsx";
+import Main from "./components/main/Main.jsx";
 import Footer from "./components/header-footer/Footer.jsx";
 import Settings from "./components/settings-page/Settings.jsx";
-import HomePage from "./components/home-page/HomePage.jsx";
-import FlashCards from "./components/flashcards-page/FlashCards.jsx";
-// Authentication pages imports
-import Signup from "./components/authentication-pages/Signup";
-import Dashboard from "./components/authentication-pages/Dashboard";
-import Login from "./components/authentication-pages/Login";
-import PrivateRoute from "./components/authentication-pages/PrivateRoute";
-import ForgotPassword from "./components/authentication-pages/ForgotPassword";
-import UpdateProfile from "./components/authentication-pages/UpdateProfile";
+import { LoadingIcon } from "./components/icons/Icons.jsx";
 
 const App = () => {
   let theme = useTheme();
+  const [homeButtons, setHomeButtons] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://my-json-server.typicode.com/serhatci/mockdb/buttons")
+      .then((response) => response.json())
+      .then((data) => {
+        setHomeButtons(data);
+        setLoading(false);
+      });
+  }, []);
+
+  const getMainPage = isLoading ? (
+    <div className="loading-container">
+      <LoadingIcon />
+    </div>
+  ) : (
+    <Main buttons={homeButtons} />
+  );
+
   return (
     <div className="app-container" id="app-container" style={theme.backg}>
       <header className="header">
         <Header />
       </header>
-      <main className="main-page">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/flashcards" component={FlashCards} />
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <PrivateRoute path="/update-profile" component={UpdateProfile} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/forgot-password" component={ForgotPassword} />
-        </Switch>
-      </main>
+      <main className="main-page">{getMainPage}</main>
       <SettingsProvider>
         <footer className="footer">
           <Footer />
