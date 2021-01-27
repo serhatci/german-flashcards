637a7a690cf6c5a54de4ce4139e93c60428bff77
serhat <serhatciftci@gmail.com>
2021-01-27 17:35:31 +0100

    Transffer to new folder

---
+++ src/app/components/form-components/Validation.jsx
@@ -0,0 +1,27 @@
+import * as yup from "yup";
+
+export const LoginValSchema = yup.object().shape({
+  email: yup.string().email("*Invalid Email").required("*Email Required"),
+  password: yup
+    .string()
+    .min(4, "*Password too Short")
+    .max(50, "Come on!... Keep it shorter!..")
+    .required("*Password Required"),
+});
+
+export const SignupValSchema = yup.object().shape({
+  email: yup.string().email("*Invalid Email").required("*Email Required"),
+  password: yup
+    .string()
+    .min(4, "*Password too Short")
+    .max(50, "Come on!.. Keep it shorter!..")
+    .required("*Password Required"),
+  passconfirm: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "*Passwords must match")
+    .required("*Confirmation Required"),
+});
+
+export const resetPasswordValSchema = yup.object().shape({
+  email: yup.string().email("*Invalid Email").required("*Email Required"),
+});
