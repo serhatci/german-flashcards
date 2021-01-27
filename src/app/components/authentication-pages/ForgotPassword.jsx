639045dea12356cd6870afa82df4b821b4b7c8aa
serhat <serhatciftci@gmail.com>
2021-01-27 21:19:30 +0100

    Update success message and corrections

--- src/app/components/authentication-pages/ForgotPassword.jsx
+++ src/app/components/authentication-pages/ForgotPassword.jsx
@@ -11,42 +11,67 @@ const ForgotPassword = () => {
   const { resetPassword } = useAuth();
   const [connError, setConnError] = useState("");
   const [success, setSuccess] = useState(false);
-  const history = useHistory();
+
+  function forgotPassForm() {
+    return (
+      <>
+        <Formik
+          initialValues={{
+            email: "",
+          }}
+          validationSchema={resetPasswordValSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            resetPassword(values.email).then(
+              () => {
+                setSuccess(true);
+                setConnError("");
+              },
+              (error) => {
+                let errorMessage = error.message;
+                setConnError(errorMessage);
+                setSuccess(false);
+              }
+            );
+            setSubmitting(false);
+          }}
+        >
+          <Form id="reset-password">
+            <Input label="Email:" name="email" type="email" key="email" />
+            <SubmitButton />
+          </Form>
+        </Formik>
+        <div className="links-container">
+          Do you need to{" "}
+          <Link to="/signup">
+            <strong>Sign Up?</strong>
+          </Link>
+          <br></br>
+          or want to{" "}
+          <Link to="/login">
+            <strong>Log In?</strong>
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  function successMessage() {
+    return (
+      <>
+        <p className="success-message">
+          Your new password has been sent to your email address
+        </p>
+        <p>
+          <Link to="/login">Log In?</Link>
+        </p>
+      </>
+    );
+  }
 
   return (
     <div className="auth-container">
       <div className="auth-error">{connError}</div>
-      <Formik
-        initialValues={{
-          email: "",
-        }}
-        validationSchema={resetPasswordValSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          resetPassword(values.email)
-            .then(setSuccess(true))
-            .catch((error) => {
-              let errorMessage = error.message;
-              setConnError(errorMessage);
-            });
-          setSubmitting(false);
-        }}
-      >
-        <Form id="reset-password">
-          <Input label="Email:" name="email" type="email" key="email" />
-          <SubmitButton />
-        </Form>
-      </Formik>
-      <div className="links-container">
-        Do you need to{" "}
-        <Link to="/signup">
-          <strong>Sign Up?</strong>
-        </Link>
-        <br></br>
-        or want to{" "}
-        <Link to="/Login">
-          <strong>Log In?</strong>
-        </Link>
-      </div>
+      {success ? successMessage() : forgotPassForm()}
     </div>
   );
 };
