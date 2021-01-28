ae4b77272d32c13d5ae41e833520e6f682af99dc
serhat <serhatciftci@gmail.com>
2021-01-28 18:19:27 +0100

    Add getStyle function

--- src/app/components/form-components/Input.jsx
+++ src/app/components/form-components/Input.jsx
@@ -3,17 +3,24 @@ import { useField } from "formik";
 
 const Input = ({ label, ...props }) => {
   const [field, meta] = useField(props);
+
+  const getStyle = () => {
+    if (!meta.touched) {
+      return "";
+    } else if (meta.touched && meta.error) {
+      return "input-error";
+    } else {
+      return "input-success";
+    }
+  };
+
   return (
     <>
       <label htmlFor={props.id || props.name}>{label}</label>{" "}
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
       ) : null}
-      <input
-        className={meta.touched && meta.error ? "input-error" : ""}
-        {...field}
-        {...props}
-      />
+      <input className={getStyle()} {...field} {...props} />
     </>
   );
 };
