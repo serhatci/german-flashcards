acbd597e44d7badbcea2252fe79bb4bcd470b531
serhat <serhatciftci@gmail.com>
2021-02-09 19:47:21 +0100

    Transfer link components to relevant icons

--- src/app/components/header-footer/Header.jsx
+++ src/app/components/header-footer/Header.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { HomeIcon, UserIcon } from "../icons/Icons.jsx";
 import "./header-footer.css";
-import { UserProvider } from "../../contexts/UserContext";
 
 const Header = () => {
   return (
@@ -10,15 +8,9 @@ const Header = () => {
       className="flex-horizontal header-footer-container"
       id="header-container"
     >
-      <Link to="/">
-        <HomeIcon />
-      </Link>
+      <HomeIcon />
       <h1 id="app-title">German Flashcards</h1>
-      <Link to="/login">
-        <UserProvider>
-          <UserIcon />
-        </UserProvider>
-      </Link>
+      <UserIcon />
     </div>
   );
 };
