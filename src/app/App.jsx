c6ebe9fbaabb7ab0ab2cc50b76798bc8a7b34f6f
serhat <serhatciftci@gmail.com>
2021-02-12 15:46:12 +0100

    Transfer fetch method to Homeage comp

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { SettingsProvider } from "./contexts/SettingsContext";
 import { ShuffleProvider } from "./contexts/ShuffleContext";
 import { useTheme } from "./contexts/ThemeContext";
@@ -9,69 +9,22 @@ import Header from "./components/header-footer/Header.jsx";
 import Main from "./components/main/Main.jsx";
 import Footer from "./components/header-footer/Footer.jsx";
 import Settings from "./components/settings-page/Settings.jsx";
-import { LoadingIcon } from "./components/icons/Icons.jsx";
-import { useAuth } from "./contexts/AuthContext";
 
 const App = () => {
   let theme = useTheme();
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const { currentUser } = useAuth();
-
-  useEffect(() => {
-    setLoading(true);
-    fetch("http://127.0.0.1:5000/api/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ uid: currentUser ? currentUser.uid : "guest" }),
-    })
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error(
-          "Sorry!... Something went terribly wrong, please try again later..."
-        );
-      })
-      .then((data) => {
-        for (let title in data) {
-          localStorage.setItem(title, JSON.stringify(data[title]));
-        }
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
-  }, [currentUser]);
-
-  const getMainPage = () => {
-    if (isLoading) {
-      return (
-        <div className="loading-container">
-          <LoadingIcon />
-        </div>
-      );
-    } else if (error) {
-      return <div className="loading-container">{error.message}</div>;
-    }
-    return <Main />;
-  };
 
   return (
     <ShuffleProvider>
-      <SettingsProvider>
-        <div className="app-container" id="app-container" style={theme.backg}>
-          <header className="header">
+      <SettingsProvider >
+        <div style={theme.backg} className="app-container" id="app-container" >
+          <header className="header" >
             <Header />
           </header>
-
-          <main className="main-page">{getMainPage()}</main>
-
-          <footer className="footer">
+          <main className="main-page" >
+            <Main />
+          </main><footer className="footer" >
             <Footer />
-          </footer>
-          <Settings />
+          </footer> <Settings />
         </div>
       </SettingsProvider>
     </ShuffleProvider>
