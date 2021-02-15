c56939ed3a2a0dc2d6d5125a6d552507e897227e
serhat <serhatciftci@gmail.com>
2021-02-15 20:27:30 +0100

    Add success message and use effect

--- src/app/components/authentication-pages/Login.jsx
+++ src/app/components/authentication-pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Formik, Form } from "formik";
 import { LoginValSchema } from "../form-components/Validation";
 import { Link, useHistory } from "react-router-dom";
@@ -10,48 +10,68 @@ import SubmitButton from "../form-components/SubmitButton";
 const Login = () => {
   const { login } = useAuth();
   const [connError, setConnError] = useState("");
+  const [success, setSuccess] = useState(false);
   const history = useHistory();
 
-  return (
-    <div className="auth-container">
-      <div className="auth-error">{connError}</div>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        validationSchema={LoginValSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          login(values.email, values.password).then(
-            () => {
-              history.push("/");
-              setConnError("");
-            },
-            (error) => {
-              let errorMessage = error.message;
-              setConnError(errorMessage);
-            }
-          );
-          setSubmitting(false);
-        }}
-      >
-        <Form id="login">
-          <Input label="Email:" name="email" type="email" />
-          <Input label="Password:" name="password" type="password" />
-          <SubmitButton />
-        </Form>
-      </Formik>
+  useEffect(() => {
+    if (!success) return
+
+    var timer = setTimeout(() => {
+      history.push("/");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [success, history])
+
+  function loginForm() {
+    return (<><Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={LoginValSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        login(values.email, values.password).then(
+          () => {
+            localStorage.clear()
+            setSuccess(true)
+            setConnError("");
+          },
+          (error) => {
+            setConnError(error.message);
+            setSuccess(false)
+          }
+        );
+        setSubmitting(false);
+      }}
+    >
+      <Form id="login">
+        <Input label="Email:" name="email" type="email" />
+        <Input label="Password:" name="password" type="password" />
+        <SubmitButton />
+      </Form>
+    </Formik>
       <div className="links-container">
         Do you need an account?{" "}
         <Link to="/signup">
           <strong>Sign Up</strong>
         </Link>
         <br></br>
-        or did you{" "}
+      or did you{" "}
         <Link to="/forgot-password">
           <strong>forget your password?</strong>
         </Link>
-      </div>
+      </div></>)
+  }
+
+  function successMessage() {
+    return <p className="success-message">You have successfully logged in!</p>;
+  }
+
+  return (
+    <div className="auth-container">
+      <div className="auth-error">{connError}</div>
+      {success ? successMessage() : loginForm()}
     </div>
   );
 };
