bd66a5623625cd65d2350dc1860a01882e30cf8a
serhat <serhatciftci@gmail.com>
2021-02-15 11:43:35 +0100

    Add username and fix passconfirm to camelcase

--- src/app/components/form-components/Validation.jsx
+++ src/app/components/form-components/Validation.jsx
@@ -10,13 +10,17 @@ export const LoginValSchema = yup.object().shape({
 });
 
 export const SignupValSchema = yup.object().shape({
+  username: yup
+    .string()
+    .max(30, "Come on!.. Keep it shorter!..")
+    .required("*Provide a funny name please"),
   email: yup.string().email("*Invalid Email").required("*Email Required"),
   password: yup
     .string()
     .min(4, "*Password too Short")
     .max(50, "Come on!.. Keep it shorter!..")
     .required("*Password Required"),
-  passconfirm: yup
+  passConfirm: yup
     .string()
     .oneOf([yup.ref("password"), null], "*Passwords must match")
     .required("*Required"),
