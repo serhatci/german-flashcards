d9af88c0d4ce2b2c21ee845b36556224f4d76f16
serhat <serhatciftci@gmail.com>
2021-02-15 23:08:17 +0100

    Add password update schema

--- src/app/components/form-components/Validation.jsx
+++ src/app/components/form-components/Validation.jsx
@@ -29,3 +29,16 @@ export const SignupValSchema = yup.object().shape({
 export const resetPasswordValSchema = yup.object().shape({
   email: yup.string().email("*Invalid Email").required("*Email Required"),
 });
+
+export const UpdatePassValSchema = yup.object().shape({
+  password: yup
+    .string()
+    .min(4, "*Password too Short")
+    .max(50, "Come on!.. Keep it shorter!..")
+    .required("*Password Required"),
+  passConfirm: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "*Passwords must match")
+    .required("*Required"),
+});
+
