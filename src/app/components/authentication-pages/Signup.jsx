ce6f55ecfb1369ef1c23a1b61f0db1b335dc36d5
serhat <serhatciftci@gmail.com>
2021-02-16 21:28:55 +0100

    Refactor Signup comp

--- src/app/components/authentication-pages/Signup.jsx
+++ src/app/components/authentication-pages/Signup.jsx
@@ -9,7 +9,6 @@ import { useAuth } from "../../contexts/AuthContext";
 import "./authentication.css";
 
 const Signup = () => {
-  const { signup, deleteUser } = useAuth();
   const [connError, setConnError] = useState("");
   const [success, setSuccess] = useState(false);
   const history = useHistory();
@@ -23,98 +22,104 @@ const Signup = () => {
     return () => clearTimeout(timer);
   }, [history, success]);
 
-  function signupForm() {
-    async function submitForm(values, setSubmitting) {
-      setConnError("");
-      try {
-        var newUser = await signup(values.email, values.password);
-      } catch (err) {
-        setConnError(err.message);
-        setSuccess(false);
-      }
-
-      if (!newUser.user) return;
-
-      try {
-        await addUserToDB(newUser.user, values.username);
-      } catch (err) {
-        await deleteUser(newUser.user);
-        setConnError(err.message);
-        setSuccess(false);
-      }
-
-      setSubmitting(true);
-    }
-
-    async function addUserToDB(newUser, username) {
-      const options = {
-        method: "POST",
-        headers: { "Content-Type": "application/json;charset=utf-8" },
-        body: JSON.stringify({
-          id: newUser.uid,
-          username: username,
-          email: newUser.email,
-        }),
-      };
-
-      const response = await fetch(
-        "http://127.0.0.1:5000/api/add-user",
-        options
-      );
-
-      if (response.ok) {
-        localStorage.clear();
-        setSuccess(true);
-      } else {
-        const err = await response.json();
-        throw new Error(err.message);
-      }
-    }
-
-    return (
-      <>
-        <Formik
-          initialValues={{
-            username: "",
-            email: "",
-            password: "",
-            passConfirm: "",
-          }}
-          validationSchema={SignupValSchema}
-          onSubmit={(values, { setSubmitting }) =>
-            submitForm(values, setSubmitting)
-          }>
-          <Form id="signup">
-            <Input label="User Name:" name="username" type="text" />
-            <Input label="Email:" name="email" type="email" />
-            <Input label="Password:" name="password" type="password" />
-            <Input
-              label="Confirm Password:"
-              name="passConfirm"
-              type="password"
-            />
-            <SubmitButton />
-          </Form>
-        </Formik>
-        <div className="links-container">
-          Already have an account?{" "}
-          <Link to="/login">
-            <strong>Log In</strong>
-          </Link>
-        </div>
-      </>
-    );
-  }
-
-  function successMessage() {
-    return <p className="success-message">You are successfully registered!</p>;
-  }
-
   return (
     <div className="auth-container">
       <div className="auth-error">{connError}</div>
-      {success ? successMessage() : signupForm()}
+      {success ? (
+        <SuccessMessage />
+      ) : (
+        <SignupForm connErr={setConnError} success={setSuccess} />
+      )}
     </div>
+  );
+};
+
+const SuccessMessage = () => {
+  return <p className="success-message">You are successfully registered!</p>;
+};
+
+const SignupForm = (props) => {
+  const { signup, deleteUser } = useAuth();
+
+  async function submitForm(values, setSubmitting) {
+    props.connErr("");
+    try {
+      var newUser = await signup(values.email, values.password);
+    } catch (err) {
+      props.connErr(err.message);
+      props.success(false);
+    }
+
+    if (!newUser.user) return;
+
+    try {
+      await addUserToDB(newUser.user, values.username);
+    } catch (err) {
+      await deleteUser(newUser.user);
+      props.connErr(err.message);
+      props.success(false);
+    }
+
+    setSubmitting(true);
+  }
+
+  async function addUserToDB(newUser, username) {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json;charset=utf-8" },
+      body: JSON.stringify({
+        id: newUser.uid,
+        username: username,
+        email: newUser.email,
+      }),
+    };
+
+    const response = await fetch(
+      "http://127.0.0.1:5000/api/add-user",
+      options
+    );
+
+    if (response.ok) {
+      localStorage.clear();
+      props.success(true);
+    } else {
+      const err = await response.json();
+      throw new Error(err.message);
+    }
+  }
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+          passConfirm: "",
+        }}
+        validationSchema={SignupValSchema}
+        onSubmit={(values, { setSubmitting }) =>
+          submitForm(values, setSubmitting)
+        }>
+        <Form id="signup">
+          <Input label="User Name:" name="username" type="text" />
+          <Input label="Email:" name="email" type="email" />
+          <Input label="Password:" name="password" type="password" />
+          <Input
+            label="Confirm Password:"
+            name="passConfirm"
+            type="password"
+          />
+          <SubmitButton />
+        </Form>
+      </Formik>
+      <div className="links-container">
+        Already have an account?{" "}
+        <Link to="/login">
+          <strong>Log In</strong>
+        </Link>
+      </div>
+    </>
   );
 };
 
