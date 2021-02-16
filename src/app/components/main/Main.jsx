1f5257a993e75eb92dcfd1b2e6196c3b1b5b89be
serhat <serhatciftci@gmail.com>
2021-02-16 21:56:12 +0100

    Remove private routes

--- src/app/components/main/Main.jsx
+++ src/app/components/main/Main.jsx
@@ -4,10 +4,8 @@ import HomePage from "../home-page/HomePage.jsx";
 import FlashCards from "../flashcards-page/FlashCards.jsx";
 // Authentication pages imports
 import Signup from "../authentication-pages/Signup";
-import Dashboard from "../authentication-pages/Dashboard";
 import Login from "../authentication-pages/Login";
 import UserPage from "../authentication-pages/UserPage";
-import PrivateRoute from "../authentication-pages/PrivateRoute";
 import ForgotPassword from "../authentication-pages/ForgotPassword";
 import UpdatePassword from "../authentication-pages/UpdatePassword";
 
@@ -16,8 +14,7 @@ const Main = () => {
     <Switch>
       <Route path="/" exact component={HomePage} />
       <Route path="/flashcards/:title" component={FlashCards} />
-      <PrivateRoute path="/dashboard" component={Dashboard} />
-      <PrivateRoute path="/update-profile" component={UpdatePassword} />
+      <Route path="/update-password" component={UpdatePassword} />
       <Route path="/signup" component={Signup} />
       <Route path="/login" component={Login} />
       <Route path="/user-page" component={UserPage} />
