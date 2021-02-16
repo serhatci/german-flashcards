a3b85b0486c23e42ac6b886d4003d89c08346ea4
serhat <serhatciftci@gmail.com>
2021-02-16 21:08:46 +0100

    Refactor UpdatePassword comp

--- src/app/components/authentication-pages/UpdatePassword.jsx
+++ src/app/components/authentication-pages/UpdatePassword.jsx
@@ -8,67 +8,81 @@ import SubmitButton from "../form-components/SubmitButton";
 import { useAuth } from "../../contexts/AuthContext";
 
 const UpdatePassword = () => {
-  const { currentUser, updatePassword } = useAuth();
+  const { currentUser } = useAuth();
   const [connError, setConnError] = useState("");
   const [success, setSuccess] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
-    if (!success) return
+    if (!success) return;
 
     const timer = setTimeout(() => {
       history.push("/");
     }, 2000);
     return () => clearTimeout(timer);
-
   }, [success, history]);
-
-  function profileUpdateForm() {
-    return (
-      <>
-        <Formik
-          initialValues={{
-            password: "",
-            passConfirm: "",
-          }}
-          validationSchema={UpdatePassValSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setConnError("")
-            updatePassword(values.password)
-              .then(() => setSuccess(true))
-              .catch((err) => {
-                setConnError(err.message);
-                setSuccess(false)
-              })
-
-            setSubmitting(false);
-          }}
-        >
-          <Form id="updatePassword">
-            <Input label="New Password:" name="password" type="password" />
-            <Input label="Confirm Password:" name="passConfirm" type="password" />
-            <SubmitButton />
-          </Form>
-        </Formik>
-        <div className="links-container">
-          <Link to="/user-page">
-            <strong>GO BACK</strong>
-          </Link>
-        </div>
-      </>
-    );
-  }
-
-  function successMessage() {
-    return <p className="success-message">You have successfully updated your password!</p>;
-  }
 
   return (
     <div className="auth-container">
       <div className="auth-error">{connError}</div>
       <div className="update-profile-user">{currentUser.email}</div>
-      {success ? successMessage() : profileUpdateForm()}
+      {success ? (
+        <SuccessMessage />
+      ) : (
+        <ProfileUpdateForm setConErr={setConnError} success={setSuccess} />
+      )}
     </div>
+  );
+};
+
+const SuccessMessage = () => {
+  return (
+    <p className="success-message">
+      You have successfully updated your password!
+    </p>
+  );
+};
+
+const ProfileUpdateForm = (props) => {
+  const { updatePassword } = useAuth();
+
+  function submitForm(values) {
+    props.setConnErr("");
+    updatePassword(values.password)
+      .then(() => props.success(true))
+      .catch((err) => {
+        props.setConnErr(err.message);
+        props.success(false);
+      });
+  }
+  return (
+    <>
+      <Formik
+        initialValues={{
+          password: "",
+          passConfirm: "",
+        }}
+        validationSchema={UpdatePassValSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          submitForm(values);
+          setSubmitting(false);
+        }}>
+        <Form id="updatePassword">
+          <Input label="New Password:" name="password" type="password" />
+          <Input
+            label="Confirm Password:"
+            name="passConfirm"
+            type="password"
+          />
+          <SubmitButton />
+        </Form>
+      </Formik>
+      <div className="links-container">
+        <Link to="/user-page">
+          <strong>GO BACK</strong>
+        </Link>
+      </div>
+    </>
   );
 };
 
