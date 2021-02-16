e298651e65cf211044c176547739ee1e5b13f536
serhat <serhatciftci@gmail.com>
2021-02-16 21:48:12 +0100

    Refactor ForgotPassword comp

--- src/app/components/authentication-pages/ForgotPassword.jsx
+++ src/app/components/authentication-pages/ForgotPassword.jsx
@@ -1,78 +1,85 @@
 import { useState } from "react";
 import { Formik, Form } from "formik";
-import { resetPasswordValSchema } from "../form-components/Validation";
 import { Link } from "react-router-dom";
+
+import { resetPasswordValSchema } from "../form-components/Validation";
 import Input from "../form-components/Input";
-import "./authentication.css";
 import SubmitButton from "../form-components/SubmitButton";
 import { useAuth } from "../../contexts/AuthContext";
+import "./authentication.css";
 
 const ForgotPassword = () => {
-  const { resetPassword } = useAuth();
   const [connError, setConnError] = useState("");
   const [success, setSuccess] = useState(false);
-
-  function forgotPassForm() {
-    return (
-      <>
-        <Formik
-          initialValues={{
-            email: "",
-          }}
-          validationSchema={resetPasswordValSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            resetPassword(values.email).then(
-              () => {
-                setSuccess(true);
-                setConnError("");
-              },
-              (error) => {
-                let errorMessage = error.message;
-                setConnError(errorMessage);
-                setSuccess(false);
-              }
-            );
-            setSubmitting(false);
-          }}
-        >
-          <Form id="reset-password">
-            <Input label="Email:" name="email" type="email" key="email" />
-            <SubmitButton />
-          </Form>
-        </Formik>
-        <div className="links-container">
-          Do you need to{" "}
-          <Link to="/signup">
-            <strong>Sign Up?</strong>
-          </Link>
-          <br></br>
-          or want to{" "}
-          <Link to="/login">
-            <strong>Log In?</strong>
-          </Link>
-        </div>
-      </>
-    );
-  }
-
-  function successMessage() {
-    return (
-      <>
-        <p className="success-message">
-          Your new password has been sent to your email address
-        </p>
-        <p>
-          <Link to="/login">Log In?</Link>
-        </p>
-      </>
-    );
-  }
 
   return (
     <div className="auth-container">
       <div className="auth-error">{connError}</div>
-      {success ? successMessage() : forgotPassForm()}
+      {success ? (
+        <SuccessMessage />
+      ) : (
+        <ForgotPassForm connErr={setConnError} success={setSuccess} />
+      )}
     </div>
+  );
+};
+
+const SuccessMessage = () => {
+  return (
+    <>
+      <p className="success-message">
+        Your new password has been sent to your email address
+      </p>
+      <p>
+        <Link to="/login">Log In?</Link>
+      </p>
+    </>
+  );
+};
+
+const ForgotPassForm = (props) => {
+  const { resetPassword } = useAuth();
+
+  function submitForm(values) {
+    resetPassword(values.email).then(
+      () => {
+        props.success(true);
+        props.connErr("");
+      },
+      (error) => {
+        let errorMessage = error.message;
+        props.connErr(errorMessage);
+        props.success(false);
+      }
+    );
+  }
+
+  return (
+    <>
+      <Formik
+        initialValues={{ email: "" }}
+        validationSchema={resetPasswordValSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          submitForm(values);
+          setSubmitting(false);
+        }}>
+        <Form id="reset-password">
+          <Input label="Email:" name="email" type="email" key="email" />
+          <SubmitButton />
+        </Form>
+      </Formik>
+      <div className="links-container">
+        Do you need to
+        <Link to="/signup">
+          <strong> Sign Up?</strong>
+        </Link>
+        <br></br>
+        or want to
+        <Link to="/login">
+          <strong> Log In?</strong>
+        </Link>
+      </div>
+    </>
   );
 };
 
