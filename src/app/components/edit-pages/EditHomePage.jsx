a05bfbaf1965c6ea24daace3a2c02a770f5caf0a
serhat <serhatciftci@gmail.com>
2021-02-19 23:19:22 +0100

    Remove fetching

--- src/app/components/edit-pages/EditHomePage.jsx
+++ src/app/components/edit-pages/EditHomePage.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-
 import { useAuth } from "../../contexts/AuthContext";
-import { LoadingIcon } from "../icons/Icons";
 import { useTheme, useThemeUpdate } from "../../contexts/ThemeContext";
 import {
   EditSaveButton,
   EditCancelButton,
   EditPageButton,
+  AddButton,
 } from "../buttons/EditButtons";
 import "./edit.css";
 
@@ -15,33 +14,17 @@ const EditHomePage = () => {
   const updateTheme = useThemeUpdate();
 
   const { currentUser } = useAuth();
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const [buttonTitles, setButtonTitles] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     if (theme.name === "Night") updateTheme();
-
-    const headers = {
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-        Authentication: currentUser.uid,
-      },
-    };
-
-    setLoading(true);
-    fetch("http://127.0.0.1:5000/api/", headers)
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.titles) throw new Error(data.message);
-        for (let title in data) {
-          localStorage.setItem(title, JSON.stringify(data[title]));
-        }
-        setButtons();
-      })
-      .catch((error) => setError(error.message))
-      .finally(() => setLoading(false));
-  }, [currentUser, theme, updateTheme]);
+    if (localStorage.getItem("titles")) {
+      return setButtons();
+    } else {
+      setError(true);
+    }
+  }, [theme, updateTheme, setError]);
 
   function setButtons() {
     setButtonTitles(JSON.parse(localStorage.getItem("titles")));
@@ -59,10 +42,14 @@ const EditHomePage = () => {
 
   return (
     <div className="home-page-container fade-in" id="app-homepage">
-      {isLoading || error ? (
-        <EditMessageBoard loading={isLoading} error={error} />
+      {error ? (
+        <p>Error occurred! Click HOME icon and try again..."</p>
       ) : (
-        <EditMainPage create={createButtons} theme={theme} />
+        <EditMainPage
+          create={createButtons}
+          theme={theme}
+          setButtons={setButtons}
+        />
       )}
     </div>
   );
@@ -76,17 +63,12 @@ const EditMainPage = (props) => {
         <EditCancelButton />
       </div>
       <div className="button-container" id="button-container">
+        <div className="plusIcon-container" id="plusIcon-container">
+          <AddButton setButtons={props.setButtons} />
+        </div>
         <nav aria-labelledby="content-navigation">{props.create()}</nav>
       </div>
     </>
-  );
-};
-
-const EditMessageBoard = (props) => {
-  return (
-    <div className="loading-container">
-      {props.loading ? <LoadingIcon /> : props.error}
-    </div>
   );
 };
 
