d4b1474e3b5d8160a22fdb6f6239535b1f9b3459
serhat <serhatciftci@gmail.com>
2021-02-21 17:27:23 +0100

    Add EditBox comp

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SettingsProvider } from "./contexts/SettingsContext";
 import { ShuffleProvider } from "./contexts/ShuffleContext";
 import { useTheme } from "./contexts/ThemeContext";
+import EditBox from "./components/edit-pages/EditBox";
 import "./app.css";
 
 // Main App components
@@ -15,16 +16,19 @@ const App = () => {
 
   return (
     <ShuffleProvider>
-      <SettingsProvider >
-        <div style={theme.backg} className="app-container" id="app-container" >
-          <header className="header" >
+      <SettingsProvider>
+        <div style={theme.backg} className="app-container" id="app-container">
+          <header className="header">
             <Header />
           </header>
-          <main className="main-page" >
+          <main className="main-page">
             <Main />
-          </main><footer className="footer" >
+          </main>
+          <footer className="footer">
             <Footer />
-          </footer> <Settings />
+          </footer>
+          <Settings />
+          <EditBox />
         </div>
       </SettingsProvider>
     </ShuffleProvider>
