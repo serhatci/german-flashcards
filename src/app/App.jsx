a582da0b6f848d356f9d97f335b8bc3054eb1ec1
serhat <serhatciftci@gmail.com>
2021-02-22 12:41:32 +0100

    Change old Providers to  ButtonsProvider

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { SettingsProvider } from "./contexts/SettingsContext";
-import { ShuffleProvider } from "./contexts/ShuffleContext";
 import { useTheme } from "./contexts/ThemeContext";
 import EditBox from "./components/edit-pages/EditBox";
 import "./app.css";
@@ -10,28 +8,27 @@ import Header from "./components/header-footer/Header.jsx";
 import Main from "./components/main/Main.jsx";
 import Footer from "./components/header-footer/Footer.jsx";
 import Settings from "./components/settings-page/Settings.jsx";
+import { ButtonsProvider } from "./contexts/ButtonsContext";
 
 const App = () => {
   let theme = useTheme();
 
   return (
-    <ShuffleProvider>
-      <SettingsProvider>
-        <div style={theme.backg} className="app-container" id="app-container">
-          <header className="header">
-            <Header />
-          </header>
-          <main className="main-page">
-            <Main />
-          </main>
-          <footer className="footer">
-            <Footer />
-          </footer>
-          <Settings />
-          <EditBox />
-        </div>
-      </SettingsProvider>
-    </ShuffleProvider>
+    <ButtonsProvider>
+      <div style={theme.backg} className="app-container" id="app-container">
+        <header className="header">
+          <Header />
+        </header>
+        <main className="main-page">
+          <Main />
+        </main>
+        <footer className="footer">
+          <Footer />
+        </footer>
+        <Settings />
+        <EditBox />
+      </div>
+    </ButtonsProvider>
   );
 };
 
