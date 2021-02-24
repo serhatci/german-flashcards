07dfef07569823ec168fff13764a9c52a44e07f9
serhat <serhatciftci@gmail.com>
2021-02-24 18:15:06 +0100

    Transfer all buttons from EditButtons file to here

--- src/app/components/edit-pages/EditHomePage.jsx
+++ src/app/components/edit-pages/EditHomePage.jsx
@@ -1,18 +1,21 @@
-import { EditButton, AddNewData } from "../buttons/EditButtons";
+import { useRef, useState } from "react";
+
 import { useData } from "../../contexts/DataContext";
+import { PlusIcon } from "../icons/Icons";
+import ContentEditable from "react-contenteditable";
+import {
+  correctInput,
+  toCamelCase,
+  duplicateCheck,
+} from "../edit-pages/inputCheckFunctions";
 import "./edit.css";
 
 const EditHomePage = () => {
-  const { titles } = useData();
-  const editPage = "homepage";
+  const { titles, setTitles } = useData();
 
   function createButtons() {
     return titles.map((title) => (
-      <EditButton
-        key={title.camelCase}
-        title={title.str}
-        editPage={editPage}
-      />
+      <EditButton key={title.camelCase} buttonTitle={title.str} />
     ));
   }
 
@@ -20,11 +23,94 @@ const EditHomePage = () => {
     <div className="home-page-container fade-in" id="app-homepage">
       <div className="edit-button-container" id="button-container">
         <div className="plusIcon-container" id="plusIcon-container">
-          <AddNewData editPage={editPage} />
+          <AddNewData titles={titles} setTitles={setTitles} />
         </div>
         <nav aria-labelledby="content-navigation">{createButtons()}</nav>
       </div>
     </div>
+  );
+};
+
+const EditButton = (props) => {
+  return (
+    <div className="edit-page-buttons fade-in" id="edit-buttons">
+      <DeleteHomePageTitleButton buttonTitle={props.buttonTitle} />
+      {props.buttonTitle}
+    </div>
+  );
+};
+
+const DeleteHomePageTitleButton = (props) => {
+  const { titles, setTitles } = useData();
+
+  function deleteTitle() {
+    const newHomePageTitles = titles.filter(
+      (title) => title.str !== props.buttonTitle
+    );
+    setTitles(newHomePageTitles);
+  }
+
+  return (
+    <button
+      className="edit-title-buttons delete"
+      id="delete-homepage-title-button"
+      onClick={() => deleteTitle()}>
+      DELETE
+    </button>
+  );
+};
+
+const AddNewData = (props) => {
+  const [clicked, setClicked] = useState(false);
+  return clicked ? (
+    <HomePageTitleInput setClicked={setClicked} />
+  ) : (
+    <PlusIcon setClicked={setClicked} />
+  );
+};
+
+const HomePageTitleInput = (props) => {
+  const { titles, setTitles } = useData();
+  const text = useRef("");
+
+  const handleChange = (evt) => {
+    text.current = evt.target.value;
+  };
+
+  function addTitle() {
+    const correctedText = correctInput(text.current);
+
+    if (duplicateCheck(correctedText, titles)) return;
+
+    const HomePageTitleInput = {
+      camelCase: toCamelCase(correctedText),
+      str: correctedText,
+    };
+    const newTitles = [HomePageTitleInput].concat(titles);
+    setTitles(newTitles);
+    props.setClicked(false);
+  }
+
+  return (
+    <div className="edit-page-buttons new fade-in" id="edit-page-buttons">
+      <ContentEditable
+        html={text.current}
+        onChange={handleChange}
+        style={{ outline: "0px solid transparent" }}
+      />
+      <AddButton add={addTitle} />
+    </div>
+  );
+};
+
+const AddButton = (props) => {
+  return (
+    <button
+      className="edit-title-buttons add-title"
+      id="add-title-button"
+      onClick={() => props.add()}>
+      ADD TITLE
+    </button>
   );
 };
 
