425e2f23c61287f48ead2bf3a6d4f70d4b36784c
serhat <serhatciftci@gmail.com>
2021-02-24 11:34:14 +0100

    Update all methods

--- src/app/components/buttons/EditButtons.jsx
+++ src/app/components/buttons/EditButtons.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import ContentEditable from "react-contenteditable";
 import { PlusIcon } from "../icons/Icons";
 import {
@@ -7,44 +7,54 @@ import {
   duplicateCheck,
 } from "../edit-pages/inputCheckFunctions";
 import "./buttons.css";
+import { useData } from "../../contexts/DataContext";
+import { useLocation } from "react-router-dom";
 
-export const EditPageButton = (props) => {
+export const EditButton = (props) => {
+  return (
+    <div className="edit-page-buttons fade-in" id="edit-buttons">
+      <DeleteButton title={props.title} editPage={props.editPage} />
+      {props.title}
+    </div>
+  );
+};
+
+export const DeleteButton = (props) => {
+  const { titles, setTitles } = useData();
+
   function deleteButton() {
-    const buttons = JSON.parse(localStorage.getItem("mockTitles"));
-    const newButtons = buttons.filter((button) => button.str !== props.title);
-    localStorage.setItem("mockTitles", JSON.stringify(newButtons));
-    props.setButtons();
+    if (props.editPage === "homepage") {
+      const HomePageTitleInputs = titles.filter(
+        (title) => title.str !== props.title
+      );
+      setTitles(HomePageTitleInputs);
+    }
   }
 
   return (
-    <>
-      <div
-        className="edit-page-buttons fade-in"
-        id="edit-buttons"
-        style={props.style}>
-        <EditDeleteButton deleteButton={deleteButton} />
-        {props.title}
-      </div>
-    </>
+    <button
+      className="edit-title-buttons delete"
+      id="delete-button"
+      onClick={() => deleteButton()}>
+      DELETE
+    </button>
   );
 };
 
-export const AddButton = (props) => {
+export const AddNewData = (props) => {
   const [clicked, setClicked] = useState(false);
 
-  return clicked ? (
-    <NewButton
-      title={props.title}
-      style={props.style}
-      setButtons={props.setButtons}
-      setClicked={setClicked}
-    />
-  ) : (
-    <PlusIcon setClicked={setClicked} />
-  );
+  function inputButton() {
+    if (props.editPage === "homepage") {
+      return <HomePageTitleInput setClicked={setClicked} />;
+    }
+  }
+
+  return clicked ? inputButton() : <PlusIcon setClicked={setClicked} />;
 };
 
-const NewButton = (props) => {
+const HomePageTitleInput = (props) => {
+  const { titles, setTitles } = useData();
   const text = useRef("");
 
   const handleChange = (evt) => {
@@ -52,55 +62,37 @@ const NewButton = (props) => {
   };
 
   function addTitle() {
-    if (text.current === "") return props.setClicked(false);
-
-    let buttons = JSON.parse(localStorage.getItem("mockTitles"));
     const correctedText = correctInput(text.current);
 
-    if (duplicateCheck(correctedText, buttons)) return;
+    if (duplicateCheck(correctedText, titles)) return;
 
-    const newButton = {
+    const HomePageTitleInput = {
       camelCase: toCamelCase(correctedText),
       str: correctedText,
     };
-    buttons.unshift(newButton);
-    localStorage.setItem("mockTitles", JSON.stringify(buttons));
-    props.setButtons();
+    const newTitles = [HomePageTitleInput].concat(titles);
+    setTitles(newTitles);
     props.setClicked(false);
   }
 
   return (
-    <div
-      className="edit-page-buttons new fade-in"
-      id="edit-page-buttons"
-      style={props.style}>
+    <div className="edit-page-buttons new fade-in" id="edit-page-buttons">
       <ContentEditable
         html={text.current}
         onChange={handleChange}
         style={{ outline: "0px solid transparent" }}
       />
-      <EditAddTitle addTitle={addTitle} />
+      <AddButton add={addTitle} />
     </div>
   );
 };
 
-export const EditDeleteButton = (props) => {
-  return (
-    <button
-      className="edit-title-buttons delete"
-      id="delete-button"
-      onClick={() => props.deleteButton()}>
-      DELETE
-    </button>
-  );
-};
-
-export const EditAddTitle = (props) => {
+export const AddButton = (props) => {
   return (
     <button
       className="edit-title-buttons add-title"
       id="add-title-button"
-      onClick={() => props.addTitle()}>
+      onClick={() => props.add()}>
       ADD TITLE
     </button>
   );
@@ -114,12 +106,22 @@ export const EditSaveButton = () => {
   );
 };
 
-export const EditResetButton = (props) => {
-  const initialButtons = useRef(JSON.parse(localStorage.getItem("titles")));
+export const EditResetButton = () => {
+  const { setTitles, setFlashcards } = useData();
+  const location = useLocation();
+  const initialTitles = useRef();
+  const initialFlashcards = useRef();
+
+  useEffect(() => {
+    initialTitles.current = JSON.parse(localStorage.getItem("titles"));
+    initialFlashcards.current = JSON.parse(localStorage.getItem("flashcards"));
+  }, []);
 
   function setInitialButtons() {
-    localStorage.setItem("mockTitles", JSON.stringify(initialButtons.current));
-    props.setButtons();
+    if (location.pathname.includes("/edit-homepage")) {
+      setTitles(initialTitles.current);
+    }
+    setFlashcards(initialFlashcards.current);
   }
 
   return (
