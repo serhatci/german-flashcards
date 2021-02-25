a8a8b5151f7444883ae38a0d9399f4f6cd504538
serhat <serhatciftci@gmail.com>
2021-02-25 18:20:14 +0100

    Add flashcards edit methods

--- src/app/components/edit-pages/EditDataCustomHook.jsx
+++ src/app/components/edit-pages/EditDataCustomHook.jsx
@@ -1,12 +1,15 @@
+import update from "immutability-helper";
 import { useData } from "../../contexts/DataContext";
 import {
+  correctInput,
+  duplicateFlashcardsCheck,
   correctHomePageInput,
   toCamelCase,
   duplicateTitleCheck,
 } from "../edit-pages/inputCheckFunctions";
 
 export function useEditData() {
-  const { titles, setTitles } = useData();
+  const { titles, setTitles, flashcards, setFlashcards } = useData();
 
   function deleteHomePageTitle(buttonTitle) {
     let newTitles = titles.filter((title) => title.str !== buttonTitle);
@@ -26,5 +29,85 @@ export function useEditData() {
     setTitles(newTitles);
   }
 
-  return { deleteHomePageTitle, addHomePageTitle };
+  function addFlashcard(question, questionExtra, answer, answerExtra, title) {
+    const correctedQuestion = correctInput(question);
+    const correctedQuestionExtra = correctInput(questionExtra);
+    const correctedAnswer = correctInput(answer);
+    const correctedAnswerExtra = correctInput(answerExtra);
+
+    if (!correctedQuestion || !correctedAnswer) return false;
+
+    const flashcardInput = {
+      question: correctedQuestion,
+      questionExtra: correctedQuestionExtra,
+      answer: correctedAnswer,
+      answerExtra: correctedAnswerExtra,
+    };
+
+    if (duplicateFlashcardsCheck(flashcardInput, flashcards[title]))
+      return false;
+
+    const newFlashcards = update(flashcards, {
+      [title]: { $unshift: [flashcardInput] },
+    });
+    setFlashcards(newFlashcards);
+    return true;
+  }
+
+  function editFlashcard(
+    question,
+    questionExtra,
+    answer,
+    answerExtra,
+    title,
+    card
+  ) {
+    const correctedQuestion = correctInput(question);
+    const correctedQuestionExtra = correctInput(questionExtra);
+    const correctedAnswer = correctInput(answer);
+    const correctedAnswerExtra = correctInput(answerExtra);
+
+    if (!correctedQuestion || !correctedAnswer) return false;
+
+    const flashcardInput = {
+      question: correctedQuestion,
+      questionExtra: correctedQuestionExtra,
+      answer: correctedAnswer,
+      answerExtra: correctedAnswerExtra,
+    };
+
+    if (duplicateFlashcardsCheck(flashcardInput, flashcards[title]))
+      return false;
+
+    // Adds new card
+    let newFlashcards = update(flashcards, {
+      [title]: { $unshift: [flashcardInput] },
+    });
+
+    // Deletes already edited card
+    newFlashcards[title] = newFlashcards[title].filter(
+      (titleCard) => JSON.stringify(titleCard) !== JSON.stringify(card)
+    );
+
+    setFlashcards(newFlashcards);
+    return true;
+  }
+
+  function deleteFlashcard(title, card) {
+    const newTitles = flashcards[title].filter(
+      (titleCard) => JSON.stringify(titleCard) !== JSON.stringify(card)
+    );
+    const newFlashcards = update(flashcards, {
+      [title]: { $set: newTitles },
+    });
+    setFlashcards(newFlashcards);
+  }
+
+  return {
+    deleteHomePageTitle,
+    addHomePageTitle,
+    addFlashcard,
+    editFlashcard,
+    deleteFlashcard,
+  };
 }
