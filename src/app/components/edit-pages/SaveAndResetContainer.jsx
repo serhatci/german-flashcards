b5c2a94d45591f93f5521d311d3a5757df92c3d4
serhat <serhatciftci@gmail.com>
2021-02-26 15:17:08 +0100

    Add succesMsg and saveToDB functions

--- src/app/components/edit-pages/SaveAndResetContainer.jsx
+++ src/app/components/edit-pages/SaveAndResetContainer.jsx
@@ -1,11 +1,13 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import { useData } from "../../contexts/DataContext";
 import "./edit.css";
 
 const SaveAndResetContainer = () => {
   const { setTitles, setFlashcards } = useData();
   const location = useLocation();
+  const [successMsg, setSuccessMsg] = useState();
 
   const style = location.pathname.includes("edit-")
     ? "edit-box-container show"
@@ -35,20 +37,48 @@ const SaveAndResetContainer = () => {
           initialFlashcards={initialFlashcards.current}
           location={location.pathname}
           editPage={editPage}
+          setSuccessMsg={setSuccessMsg}
         />
         <EditResetButton
           initialTitles={initialTitles.current}
           initialFlashcards={initialFlashcards.current}
           location={location.pathname}
           editPage={editPage}
+          setSuccessMsg={setSuccessMsg}
         />
+        {successMsg ? (
+          <MessageBox successMsg={successMsg} setSuccessMsg={setSuccessMsg} />
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
 };
 
 const EditSaveButton = (props) => {
+  const { currentUser } = useAuth();
   const { titles, flashcards } = useData();
+
+  const msgBody =
+    props.editPage === "homepage"
+      ? { id: currentUser.uid, topics: titles }
+      : { id: currentUser.uid, cards: flashcards };
+
+  async function saveToDB() {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json;charset=utf-8" },
+      body: JSON.stringify(msgBody),
+    };
+
+    await fetch("http://127.0.0.1:5000/api/save-data", options)
+      .then(() => {
+        saveToLocalStorage();
+        props.setSuccessMsg("Saved!");
+      })
+      .catch(() => props.setSuccessMsg("Failed to Save :(("));
+  }
 
   function saveToLocalStorage() {
     if (props.editPage === "homepage") {
@@ -61,7 +91,7 @@ const EditSaveButton = (props) => {
     <button
       className="edit-box-buttons save-changes"
       id="edit-save-button"
-      onClick={() => saveToLocalStorage()}>
+      onClick={() => saveToDB()}>
       Save Changes
     </button>
   );
@@ -73,8 +103,10 @@ const EditResetButton = (props) => {
   function setInitialButtons() {
     if (props.editPage === "homepage") {
       setTitles(props.initialTitles);
+      return props.setSuccessMsg("Reset Done!");
     }
     setFlashcards(props.initialFlashcards);
+    props.setSuccessMsg("Reset Done!");
   }
 
   return (
@@ -85,6 +117,27 @@ const EditResetButton = (props) => {
       Reset
     </button>
   );
+};
+
+const MessageBox = (props) => {
+  const getStyle = props.successMsg.includes("Failed")
+    ? "edit-message-box fade-in fail"
+    : "edit-message-box fade-in success";
+
+  useEffect(() => {
+    document.getElementById("edit-save-button").disabled = true;
+    document.getElementById("edit-reset-button").disabled = true;
+    setTimeout(() => {
+      props.setSuccessMsg("");
+    }, 3000);
+
+    return () => {
+      document.getElementById("edit-save-button").disabled = false;
+      document.getElementById("edit-reset-button").disabled = false;
+    };
+  }, [props]);
+
+  return <div className={getStyle}>{props.successMsg}</div>;
 };
 
 export default SaveAndResetContainer;
