ddb4ea62fd1673f2605f5a176943cafb4cef594e
serhat <serhatciftci@gmail.com>
2021-03-04 00:47:06 +0100

    BREAKING CHANGE: re-write functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,20 @@
 import { User } from "../models/post.js";
+import { frontendData, backendData } from "./convertFunctions.js";
 
-export const getData = async (req, res) => {
+export const getUserFlashcards = async (req, res) => {
+  let user = req.body;
+  let result = await userNotExists(user.userID);
+  if (user.userID === "guest" || result) {
+    user.userID = process.env.MASTER_UID;
+  }
+
   try {
-    const posts = await Post.find();
-    res.status(200).json(posts);
+    const data = await User.find(user, {
+      _id: 0,
+      topics: 1,
+      userName: 1,
+    });
+    res.status(200).json(frontendData(data));
   } catch (error) {
     res.status(404).json({
       message: error.message,
@@ -11,26 +22,20 @@ export const getData = async (req, res) => {
   }
 };
 
-export const getMasterData = async (req, res) => {
-  try {
-    const masterData = await User.find(
-      { userName: "master" },
-      { topics: 1, _id: 0 }
-    );
-    res.status(200).json(masterData);
-  } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
-  }
-};
+async function userNotExists(uid) {
+  const result = await User.find({ userID: uid });
+  return result.length === 0 ? true : false;
+}
 
 export const updateData = async (req, res) => {
-  const { id: _id } = req.params;
-  const post = req.body;
+  let data = req.body;
+  const new_topics = backendData(data);
   try {
-    const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
-    res.json(updatedPost);
+    const update = await User.updateOne(
+      { userID: data.userID },
+      { $set: { topics: new_topics } }
+    );
+    res.json(update);
   } catch (error) {
     res.status(409).json({
       message: error.message,
@@ -38,10 +43,23 @@ export const updateData = async (req, res) => {
   }
 };
 
+export const getMasterTopics = async () => {
+  try {
+    const data = await User.find(
+      { userName: "master" },
+      { topics: 1, _id: 0 }
+    );
+    const topics = data[0].topics;
+    return topics;
+  } catch (error) {
+    return { message: error.message };
+  }
+};
+
 export const addUser = async (req, res) => {
   const post = req.body;
   let newUser = new User(post);
-  newUser.topics = getMasterData();
+  newUser.topics = await getMasterTopics();
   try {
     await newUser.save();
     res.status(201).json(newUser);
@@ -53,10 +71,10 @@ export const addUser = async (req, res) => {
 };
 
 export const deleteUser = async (req, res) => {
-  const { id: _id } = req.params;
+  const user = req.body;
   try {
-    const deletedPost = await Post.findByIdAndRemove(_id);
-    res.json(deletedPost);
+    const deletedUser = await User.deleteOne(user);
+    res.json(deletedUser);
   } catch (error) {
     res.status(409).json({
       message: error.message,
